b82b91bb634b755c2d96ed568fb7de89e1dc26d7
Kris244212 <kyrylovets.k.m_kn24@rcit.ukr.education>
2025-09-18 15:31:12 +0300

    Додані файли лабораторних робіт

--- lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7e722d13e99bbe0782f31dd1db657bdacb3a56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6f98485c2b8de6747231b9bc27575f7938e428")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
